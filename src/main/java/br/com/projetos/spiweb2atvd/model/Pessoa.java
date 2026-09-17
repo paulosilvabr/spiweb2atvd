a8852945b5f78d4216ebf5808adae7b4837fdb84
paulosilvabr <pauloricardosousasilva10@gmail.com>
2026-09-17 02:51:03 -0300

    refactor: add some inputs

--- src/main/java/br/com/projetos/spiweb2atvd/model/Pessoa.java
+++ src/main/java/br/com/projetos/spiweb2atvd/model/Pessoa.java
@@ -8,18 +8,18 @@ import jakarta.persistence.*;
 public class Pessoa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String email;
     private String telefone;
 
     public Pessoa() {
     }
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
