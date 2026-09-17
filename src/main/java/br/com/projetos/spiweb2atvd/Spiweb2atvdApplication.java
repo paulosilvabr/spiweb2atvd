281f1a9419a1b8ac303994ad3af03d6e2a4737fc
paulosilvabr <pauloricardosousasilva10@gmail.com>
2026-09-17 01:51:54 -0300

    chore: setup inicial do projeto

--- src/main/java/br/com/projetos/spiweb2atvd/Spiweb2atvdApplication.java
+++ src/main/java/br/com/projetos/spiweb2atvd/Spiweb2atvdApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Spiweb2atvdApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(Spiweb2atvdApplication.class, args);
     }
-
 }
